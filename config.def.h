f31232f132f785b2fcd610df5bb096daa34866a7
Neeraj <nattavar@auracompany.com>
2020-08-06 18:57:08 +0530

    update key bind for opening brave browser

--- config.def.h
+++ config.def.h
@@ -73,14 +73,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] 		= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  		= { "st", NULL };
 static const char *vscodecmd[]  	= { "code", NULL };
 static const char *zoomcmd[]  		= { "zoom", NULL };
 static const char *pycharmcmd[]  	= { "pycharm", NULL };
 static const char *rangercmd[]  	= { "st", "-e", "ranger" };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "170x40", NULL };
+static const char scratchpadname[] 	= "scratchpad";
+static const char *scratchpadcmd[] 	= { "st", "-t", scratchpadname, "-g", "170x40", NULL };
+static const char *bravecmd[]  		= { "brave", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -96,6 +97,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_z,      					spawn,         	{.v = zoomcmd } },
 	{ MODKEY|ShiftMask,             XK_p,      					spawn,         	{.v = pycharmcmd } },
 	{ MODKEY|ShiftMask,             XK_f,      					spawn,         	{.v = rangercmd } },
+	{ MODKEY|ShiftMask,             XK_b,      					spawn,         	{.v = bravecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
